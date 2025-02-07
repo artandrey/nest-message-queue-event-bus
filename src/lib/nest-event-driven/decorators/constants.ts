23d0841bb38f3270b783d1e7490d604389ae58c2
Artandrey <artandrey7777@gmail.com>
2025-02-07 23:47:01 +0200

    fix: remove package prefix from SCOPE_OPTIONS_METADATA constant

--- src/lib/nest-event-driven/decorators/constants.ts
+++ src/lib/nest-event-driven/decorators/constants.ts
@@ -1,3 +1,3 @@
 export const PACKAGE_PREFIX = '__nest-event-driven';
 export const EVENTS_HANDLER_METADATA = `${PACKAGE_PREFIX}:eventsHandler`;
-export const SCOPE_OPTIONS_METADATA = `${PACKAGE_PREFIX}:scope:options`;
+export const SCOPE_OPTIONS_METADATA = 'scope:options';
