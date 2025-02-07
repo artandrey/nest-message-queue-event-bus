afa73f9f7d00327190e06bdd29d70147c84d9027
Artandrey <artandrey7777@gmail.com>
2025-02-07 21:21:02 +0200

    feat: remove DefaultPubSub and update EventBus event consumption methods

--- src/lib/nest-event-driven/interfaces/event-bus.interface.ts
+++ src/lib/nest-event-driven/interfaces/event-bus.interface.ts
@@ -14,5 +14,13 @@ export interface IEventBus<TEvent extends IEvent = IEvent> {
    * @throws Error when more than one handler is found for the event
    * @returns Promise that resolves when the event is handled
    */
-  consumeByStrictlySingleHandler(event: TEvent): Promise<void>;
+  synchronouslyConsumeByStrictlySingleHandler(event: TEvent): Promise<void>;
+  /**
+   * Consumes an event by multiple handlers. If there are no handlers available for the event type,
+   * an error will be thrown.
+   * @param event The event to be consumed
+   * @throws Error when no handlers are found for the event
+   * @returns Promise that resolves when all handlers are executed
+   */
+  synchronouslyConsumeByMultipleHandlers(event: TEvent): Promise<void>;
 }
